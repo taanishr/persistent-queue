c69389b434607cf61bee97649589438a19dbc725
Taanish R <tnreja@gmail.com>
2025-03-31 13:41:28 -0700

    Finished skeleton of engine class

--- pqueue/pqueue.h
+++ pqueue/pqueue.h
@@ -2,127 +2,246 @@
 #include <shared_mutex>
 #include <mutex>
 #include "storagebase.h"
+#include <fstream>
 
 // eventually want this to be templated instead of ints. Concrete implementation should store bitsreams use conversion methods
-template <typename T>
-class persistent_queue: private storage_base<T>
-{
-public:
-    using size_type = typename storage_base<T>::size_type;
-
-    persistent_queue():
-        storage_base<T>{0}
+namespace persistent_queue {
+    template <typename T>
+    class persistent_queue: private storage_base<T>
     {
-        head = 0;
-        tail = 0;
+    public:
+        using size_type = typename storage_base<T>::size_type;
+    
+        persistent_queue():
+            storage_base<T>{0}
+        {
+            head = 0;
+            tail = 0;
+        }
+    
+        int capacity() const;
+        T* data();
+    
+        int size() const;
+    
+        void enqueue(T i);
+        std::optional<T> dequeue();
+        std::optional<T> peek() const;
+    
+        // void enqueueBatch(int n);
+        // void dequeueBatch(int n);
+        
+        void reserve(size_type newAlloc);
+        void resize(size_type newSize, T val = T());
+    
+        // define iterators
+        class iterator;
+        iterator begin();
+        iterator end();
+    private:
+        void reserveImpl(size_type newAlloc);
+        std::shared_mutex mutex;
+        int head;
+        int tail;
+    };
+    
+    template <typename T>
+    void persistent_queue<T>::reserveImpl(size_type newAlloc) {
+        if (newAlloc <= this->space) return;
+    
+        storage_base<T> b {newAlloc};
+    
+        for (int i = head; i < head + this->sz; ++i) 
+            new (b.elem+i-head) int(this->elem[i % this->space]);
+        b.sz = this->sz;
+    
+        for (int i = 0; i < this->sz; ++i) std::destroy_at(this->elem+i);
+        this->sz = 0;
+    
+        std::swap<storage_base<T>>(static_cast<storage_base<T>&>(*this), static_cast<storage_base<T>&>(b));
+        this->head = 0;
+        this->tail = head+this->sz;
+    }
+    
+    template <typename T>
+    void persistent_queue<T>::reserve(size_type newAlloc)
+    {
+        std::lock_guard<std::shared_mutex> lockGuard {this->mutex}; // use shared_lock for reading
+        // call private impl once lock has been acquired
+        reserveImpl(newAlloc);
+    }
+    
+    template <typename T>
+    int persistent_queue<T>::size() const {
+        return this->sz;
+    }
+    
+    template <typename T>
+    int persistent_queue<T>::capacity() const {
+        return this->space;
+    }
+    
+    template <typename T>
+    T* persistent_queue<T>::data() {
+        return this->elem;
+    }
+    
+    template <typename T>
+    void persistent_queue<T>::resize(size_type newSize, T val)
+    {
+        std::lock_guard<std::shared_mutex> lockGuard {this->mutex};
+    
+        reserveImpl(newSize);
+        for (int i = this->sz; i < newSize; ++i) new (&this->elem[i]) T(val);
+    
+        for (int i = newSize; i < this->sz; ++i) std::destroy_at(&this->elem[i]);
+        this->sz = newSize;
+    }
+    
+    template <typename T>
+    void persistent_queue<T>::enqueue(T i) {
+        std::lock_guard<std::shared_mutex> lockGuard {this->mutex}; 
+        if (this->space == 0)
+            reserveImpl(8);
+    
+        if (this->sz >= this->space)
+            reserveImpl(2*this->space);
+    
+        new (this->elem+tail) int(i);
+        tail = (tail+1)%this->space;
+        this->sz++;
+    }
+    
+    template <typename T>
+    std::optional<T> persistent_queue<T>::dequeue() {
+        std::lock_guard<std::shared_mutex> lockGuard {this->mutex};
+        if (this->sz == 0)
+            return std::nullopt;
+        int rt = *(this->elem+head);
+        std::destroy_at(this->elem+head);
+        head = (head+1)%this->space;
+        this->sz--;
+        return rt;
+    }
+    
+    template <typename T>
+    std::optional<T> persistent_queue<T>::peek() const{
+        if (this->sz == 0)
+            return std::nullopt;
+        int rt = *(this->elem+head);
+        return rt;
+    }
+    
+    template <typename T>
+    class persistent_queue<T>::iterator {
+    private:
+        static constexpr int END_MARKER = -1;
+        int head;
+        int tail;
+        int pos;
+        int sz;
+        T* elem;
+    public:
+        iterator(int head, int tail, int post, int sz, T* elem) {
+    
+        }
+    
+        iterator operator--() {
+            if (pos == head)
+                return *this;
+            
+            if (pos == END_MARKER) {
+                pos = (tail-1) % sz;
+                elem = elem + pos;
+            }else {
+                pos = (pos - 1) % sz;
+                elem = elem + pos;
+            }
+            return *this;
+        }
+    
+        iterator operator++() {
+            if (pos == END_MARKER)
+                return *this;
+    
+            pos = (pos+1)%sz;
+            if (pos == tail)
+                pos = END_MARKER;
+            else
+                elem = elem + pos;
+            return *this;
+        }
+    
+    
+        T operator*() {
+            return *(elem+pos);
+        }
+    
+        T* operator->() {
+            return elem+pos;
+        }
+    
+        bool operator==(persistent_queue<T>::iterator& other) {
+            return pos == other.pos;
+        }
+    
+        bool operator!=(persistent_queue<T>::iterator& other) {
+            return !(this == &other);
+        }
+    
+    };
+
+
+
+
+    // work flow:
+    // enqueue/dequeue (wrapper around data structure) 
+    // flush (flushes all in memory stuff to database manually (should be automatic though)
+    // serialize (converts data structure into in memory stuff);
+    // buffer (gets current buffer (in memory data structure)
+    template <typename Container, typename T>
+    class Engine {
+        public:
+            Engine(Container memory_buffer, std::ofstream file);
+
+            unsigned long buffer_size = 64;
+
+            void enqueueBatch();
+
+            void enqueue();
+
+            T dequeue();
+
+            T peek();
+
+            // flush to file
+            void flush();
+            
+            const Container& buffer();
+
+            // serialize data
+            void serialize();
+        private:
+            Container memory_buffer;
+            std::ofstream file;
+    };
+
+    template<typename Container, typename T>
+    Engine<Container, T>::Engine(Container memory_buffer, std::ofstream file) {
+
     }
 
-    int capacity() const;
-    T* data();
+    template <typename Container, typename T>
+    void Engine<Container, T>::enqueue() {
 
-    int size() const;
+    }
 
-    void enqueue(T i);
-    std::optional<T> dequeue();
-    std::optional<T> peek() const;
+    template <typename Container, typename T>
+    T Engine<Container, T>::dequeue() {
 
-    // void enqueueBatch(int n);
-    // void dequeueBatch(int n);
-    
-    void reserve(size_type newAlloc);
-    void resize(size_type newSize, T val = T());
+    }
 
-    // define iterators
-private:
-    void reserveImpl(size_type newAlloc);
-    std::shared_mutex mutex;
-    int head;
-    int tail;
-};
-
-template <typename T>
-void persistent_queue<T>::reserveImpl(size_type newAlloc) {
-    if (newAlloc <= this->space) return;
-
-    storage_base<T> b {newAlloc};
-
-    for (int i = head; i < head + this->sz; ++i) 
-        new (b.elem+i-head) int(this->elem[i % this->space]);
-    b.sz = this->sz;
-
-    for (int i = 0; i < this->sz; ++i) std::destroy_at(this->elem+i);
-    this->sz = 0;
-
-    std::swap<storage_base<T>>(static_cast<storage_base<T>&>(*this), static_cast<storage_base<T>&>(b));
-    this->head = 0;
-    this->tail = head+this->sz;
-}
-
-template <typename T>
-void persistent_queue<T>::reserve(size_type newAlloc)
-{
-    std::lock_guard<std::shared_mutex> lockGuard {this->mutex}; // use shared_lock for reading
-    // call private impl once lock has been acquired
-    reserveImpl(newAlloc);
-}
-
-template <typename T>
-int persistent_queue<T>::size() const {
-    return this->sz;
-}
-
-template <typename T>
-int persistent_queue<T>::capacity() const {
-    return this->space;
-}
-
-template <typename T>
-T* persistent_queue<T>::data() {
-    return this->elem;
-}
-
-template <typename T>
-void persistent_queue<T>::resize(size_type newSize, T val)
-{
-    std::lock_guard<std::shared_mutex> lockGuard {this->mutex};
-
-    reserveImpl(newSize);
-    for (int i = this->sz; i < newSize; ++i) new (&this->elem[i]) T(val);
-
-    for (int i = newSize; i < this->sz; ++i) std::destroy_at(&this->elem[i]);
-    this->sz = newSize;
-}
-
-template <typename T>
-void persistent_queue<T>::enqueue(T i) {
-    std::lock_guard<std::shared_mutex> lockGuard {this->mutex}; 
-    if (this->space == 0)
-        reserveImpl(8);
-
-    if (this->sz >= this->space)
-        reserveImpl(2*this->space);
-
-    new (this->elem+tail) int(i);
-    tail = (tail+1)%this->space;
-    this->sz++;
-}
-
-template <typename T>
-std::optional<T> persistent_queue<T>::dequeue() {
-    std::lock_guard<std::shared_mutex> lockGuard {this->mutex};
-    if (this->sz == 0)
-        return std::nullopt;
-    int rt = *(this->elem+head);
-    std::destroy_at(this->elem+head);
-    head = (head+1)%this->space;
-    this->sz--;
-    return rt;
-}
-
-template <typename T>
-std::optional<T> persistent_queue<T>::peek() const{
-    if (this->sz == 0)
-        return std::nullopt;
-    int rt = *(this->elem+head);
-    return rt;
-}
+    template <typename Container, typename T>
+    void Engine<Container,T>::enqueueBatch() {
+    }
+};    
